b5e888920fd071f4dda13c6e734e310ee02fa267
joe-alfaro <josephlalfaro@gmail.com>
2019-05-17 08:30:43 -0700

    write server landing page tests

---
+++ api/server.tests.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+//const server = require('./server');
+//const db = require('../data/dbConfig');
+
+describe(`get('/')`, () => {
+  it('returns a 200 status code', async => {
+    let response = await request(server).get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('returns JSON data', async () => {
+    const response = await request(server).get('/');
+    expect(response.type).toBe('application/json');
+  });
+
+  it(`returns 'Server Home' in the response body message`, () => {
+    const response = await request(server).get('/');
+    expect(response.body.message).toBe('Server Home');
+  })
+})
