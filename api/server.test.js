07012ef4af2ee23fc79408c8048873454631ee9c
joe-alfaro <josephlalfaro@gmail.com>
2019-05-17 08:43:14 -0700

    renamed test files to .test.js added database configuration

--- api/server.test.js
+++ api/server.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 //const server = require('./server');
-//const db = require('../data/dbConfig');
+const db = require('../data/dbConfig');
 
 describe(`get('/')`, () => {
-  it('returns a 200 status code', async => {
+  it('returns a 200 status code', async () => {
     let response = await request(server).get('/');
     expect(res.status).toBe(200);
   });
@@ -13,7 +13,7 @@ describe(`get('/')`, () => {
     expect(response.type).toBe('application/json');
   });
 
-  it(`returns 'Server Home' in the response body message`, () => {
+  it(`returns 'Server Home' in the response body message`, async () => {
     const response = await request(server).get('/');
     expect(response.body.message).toBe('Server Home');
   })
